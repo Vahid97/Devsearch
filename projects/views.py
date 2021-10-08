08b09f7035ffa90c253ea2d6f0925e6928c5701e
Vahid97 <vahidefendi5@gmail.com>
2021-10-08 20:55:41 +0400

    Signals

---
+++ projects/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, redirect
+from .models import Project
+from .forms import ProjectForm
+
+def projects(request):
+
+    queryProjects = Project.objects.all()
+
+    context = {
+        'queryProjects':queryProjects
+    }
+    return render(request, 'projects/projects.html', context)
+
+def singleProjects(request, pk):
+    querySingle = Project.objects.get(id=pk)
+
+
+    return render(request, 'projects/single-project.html', {'querySingle':querySingle})
+
+def createProject(request):
+    form = ProjectForm()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+        
+
+    context = {'form':form}
+    return render(request, 'projects/project_form.html', context)
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+        
+
+    context = {'form':form}
+    return render(request, 'projects/project_form.html', context)
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'object':project}
+    return render(request, 'projects/delete_template.html', context)
+
+
+
+
